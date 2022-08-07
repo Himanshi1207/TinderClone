876888824bba510045bf2f29b7715186da3b0852
Himanshi <kanojiahimanshi1221@gmail.com>
2022-08-07 21:26:57 +0530

    created Tinder clone

---
+++ src/component/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PersonIcon from '@mui/icons-material/Person';
+import ForumIcon from '@mui/icons-material/Forum';
+import './Header.css'
+import { Icon, IconButton } from '@mui/material';
+function Header() {
+  return (
+      <div className='tinder-header'>
+        <IconButton className='header-icons' fontSize='large'>
+            <PersonIcon className='personIcon' fontSize='large'/>
+        </IconButton>
+        <img src='https://www.logo.wine/a/logo/Tinder_(app)/Tinder_(app)-Flame-Logo.wine.svg'/>
+        <IconButton className='header-icons' fontSize='large'>
+            <ForumIcon className='personIcon' fontSize='large'/>
+        </IconButton>
+        </div>
+  )
+}
+
+export default Header
---
+++ src/component/TinderCards.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import TinderCard from 'react-tinder-card'
+import './TinderCard.css'
+function TinderCards() {
+    // hooks are functions that can be used in react components.
+    // they are similar to start and props, but they are not
+    const [cars, setCars]=React.useState([
+        {
+            name:'Tesla',
+            url:'https://media.wired.com/photos/5dd828ba9ac14a0008116983/4:3/w_1920,h_1440,c_limit/Transpo_TeslaStoryOne_Cybertruck-5.jpg'
+        },
+        {
+            name:'BMW',
+            url:'https://www.topgear.com/sites/default/files/2022/06/1-BMW-3-Series.jpg'
+        },
+        {
+            name:'mercedes benz',
+            url:'https://images.news18.com/ibnlive/uploads/2020/01/The-Mercedes-Benz-Vision-AVTR-concept-car-inspired-by-the-Avatar-movies-at-ces-2020.jpg?impolicy=website&width=0&height=0'
+        },
+        {
+            name:'lykan',
+            url:'https://www.motorbiscuit.com/wp-content/uploads/2020/06/Lykan-Hypersport-cover.jpg?w=1024&h=512'
+        }
+    ]);
+  return (
+    <div className='card-container'>
+        {
+            cars.map(car=>(
+                <TinderCard
+                className='swipe'
+                key={car.name}
+                preventSwipe={['up','down']}
+                >
+                <div 
+                style={{backgroundImage:`url(${car.url})`}}
+                className='card'>
+                    <h2>{car.name}</h2>
+                </div>
+                </TinderCard>
+            ))
+        }
+    </div>
+  )
+}
+
+export default TinderCards
